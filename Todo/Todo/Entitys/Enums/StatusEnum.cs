18a5499e53091798da9e9373a504cf52276bbb2d
ruilonbl <ruilonbl@gmail.com>
2024-03-18 21:52:37 -0300

    Declarando entidades

---
+++ Todo/Todo/Entitys/Enums/StatusEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Todo.Entitys.Enums
+{
+    public enum StatusEnum
+    {
+        [Description("A fazer")]
+        Todo = 0,
+
+        [Description("Fazendo")]
+        Doing = 1,
+
+        [Description("Concluido")]
+        Done = 2,
+    }
+}
---
+++ Todo/Todo/Entitys/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.Entitys
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string UserName { get; set; }
+        public int UserLastName { get; set; }
+        public int UserCode { get; set; }
+    }
+}
